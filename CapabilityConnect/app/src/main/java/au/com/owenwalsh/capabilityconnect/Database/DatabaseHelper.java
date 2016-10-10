9b5b831a582269bee13d4dc4bd6f24c7fb68f207
owen walsh <owen walsh>
2016-10-11 10:54:56 +1100

    Init Commit

---
+++ CapabilityConnect/app/src/main/java/au/com/owenwalsh/capabilityconnect/Database/DatabaseHelper.java
@@ -0,0 +1,7 @@
+package au.com.owenwalsh.capabilityconnect.Database;
+
+/**
+ * Created by owenw on 11/10/2016.
+ */
+public class DatabaseHelper {
+}
---
+++ CapabilityConnect/app/src/main/java/au/com/owenwalsh/capabilityconnect/Model/Student.java
@@ -0,0 +1,7 @@
+package au.com.owenwalsh.capabilityconnect.Model;
+
+/**
+ * Created by owenw on 11/10/2016.
+ */
+public class Student {
+}
