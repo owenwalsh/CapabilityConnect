1ba92bdc7507cd320b86938a59f94274cd07adb0
owen walsh <owen walsh>
2016-10-11 19:24:36 +1100

    BUG FIX: Splash Screen Working

--- CapabilityConnect/app/src/main/java/au/com/owenwalsh/capabilityconnect/View/SplashActivity.java
+++ CapabilityConnect/app/src/main/java/au/com/owenwalsh/capabilityconnect/View/SplashActivity.java
@@ -5,19 +5,26 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import au.com.owenwalsh.capabilityconnect.R;
+
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                Intent i=new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
+                finish();
             }
-        }, 3000);
-        finish();
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
